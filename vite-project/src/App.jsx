9a237c1a189a8dc01c7463074f18f9a0bb643583
Eduardo Gomes <98861450+gomesbeduardo@users.noreply.github.com>
2024-08-31 05:59:02 -0300

    Add files via upload

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,65 +1,15 @@
-import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/Header";
-
 import styles from "./App.module.css";
 import "./global.css";
-import { Posts } from "./components/Post";
-
-const posts = [
-  {
-    id: 1,
-    author: "gomesbeduardo",
-    author_url: "http://github.com/gomesbeduardo.png",
-    role: "Web Developer",
-    content: [
-      {
-        type: "paragraph",
-        content:
-          "Olá, tudo bem? Vim aqui falar da minha participação no coderace 2024! Um hackathon de 12h de duração e com diversos prêmios.",
-      },
-      { type: "paragraph", content: "Foi simplesmente fantástico!" },
-    ],
-    createdAt: new Date(),
-    likes: 10,
-    comments: [
-      {
-        id: 1,
-        author: "joaozinho",
-        content: "Eu também!",
-        createdAt: new Date("2024-08-30 22:00:00"),
-      },
-      {
-        id: 2,
-        author: "mariazinha",
-        content: "Quero ver mais!",
-        createdAt: new Date("2024-08-30 22:01:30"),
-      },
-    ],
-  },
-];
+import {Pages} from "./components/Pages";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <main>
-          {posts.map((post) => {
-            return (
-              <Posts
-                author={post.author}
-                content={post.content}
-                published={post.published}
-                key={post.id}
-                {...post}
-              />
-            );
-          })}
-        </main>
-      </div>
-    </div>
-  );
+    <>
+      <Header/>
+      <Pages/>
+    </>
+  )
 }
 
-export default App;
+export default App
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import styles from "./Header.module.css";
+import {HouseSimple,Books,User} from "@phosphor-icons/react"
 
-import igniteLogo from "../assets/images/ignite-logo.svg";
+import LittlePrince from "../assets/images/Logo2-removebg-preview.png";
 
 export function Header() {
   return (
     <header className={styles.Header}>
-      <img src={igniteLogo} alt="Logotipo do Ignite" />
-      <strong>Ignite Feed</strong>
+      <img src={LittlePrince} alt="Logotipo" />
+      <strong>Universo Estrelado</strong>
+      <a href="" id="PaginaInicial" className={styles.button}><HouseSimple size={32} weight="fill" alt="Inicial"/></a>
+      <a href="" id="livros" className={styles.button}> <Books size={32} /></a>
+      <div id="Perfil" className={styles.Perfil}>
+        <a href="" id="Perfil" className={styles.perfil}><User size={32} /> </a>
+      </div>
     </header>
   );
 }
